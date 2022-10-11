f3f62893bfb3116d739585f76198feee06739e5b
Dominic Martinez <dom@dominicm.dev>
2022-10-11 17:12:50 -0400

    fix small text on mobile

--- theme.scm
+++ theme.scm
@@ -25,6 +25,8 @@
      (@ (lang "en"))
      (head
       (meta (@ (charset "utf-8")))
+      (meta (@ (name "viewport")
+               (content "width=device-width, initial-scale=1")))
       (title ,title)
       ,(stylesheet "normalize")
       ,(stylesheet "dominicm"))
