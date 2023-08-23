c6afe706056b48cd22d633a06ea37e43c5075f7d
Dominic Martinez <dom@dominicm.dev>
2023-08-23 11:20:34 -0400

    update about page

--- pages/about.scm
+++ pages/about.scm
@@ -22,22 +22,20 @@
       (h1 "Dominic Martinez")
 
       (h2 "About me")
-      (p "I'm a university student and developer interested in creating effective and
-ethical software. If you ever want to chat, my inbox is always open.")
+      (p "Welcome! I'm a university student and developer making open, user-centric
+software. I'm particularly interested in tools for thought,
+programming languages, and operating systems. If you want to chat my
+email is always open.")
 
       (h2 "Projects")
       ,(project
 	"Ham"
 	"https://sr.ht/~dominicm/ham"
 	"A modern replacement for the Perforce Jam C/C++ build system")
-      ,(project
-	"Dotfiles"
-	"https://sr.ht/~dominicm/dotfiles"
-	"A reproducible Linux configuration leveraging GNU Guix")
 
       (h2 "Contact me")
       ,(link "dom@dominicm.dev" "mailto:dom@dominicm.dev")
       (br)
-      (a (@ (rel "me") (href "https://fosstodon.org/@dominicm")) "Mastodon")
+      (a (@ (rel "me") (href "https://firefish.social/@dominicm")) "Fediverse")
       (br)
       ,(link "PGP key" "https://meta.sr.ht/~dominicm.pgp")))))
